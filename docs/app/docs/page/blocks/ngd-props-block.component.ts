5f85d948809c8a73450ee0e516dcc99134f01e52
mkolesnikov <mishkolesnikov@gmail.com>
2017-07-13 13:55:23 +0300

    feat(docs): implement props-block for inputs/outputs

--- docs/app/docs/page/blocks/ngd-props-block.component.ts
+++ docs/app/docs/page/blocks/ngd-props-block.component.ts
@@ -1,3 +1,4 @@
+
 /**
  * @license
  * Copyright Akveo. All Rights Reserved.
@@ -9,9 +10,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'ngd-props-block',
   template: `    
-    <div class="block-container">
-      <p class="block-title">Props</p>
-      <table class="table" *ngIf="classData.props.length > 0">
+    <div  class="inputs block-container" *ngIf="classInputs.length > 0">
+      <p class="block-title">Inputs</p>
+      <table  class="table table-striped">
        <thead>
          <tr>
            <td>Name</td>
@@ -20,7 +21,7 @@ import { Component, Input } from '@angular/core';
          </tr>
        </thead>
        <tbody>
-         <tr *ngFor="let prop of classData?.props">
+         <tr *ngFor="let prop of classInputs">
            <td>{{ prop.name }}</td>
            <td><code>{{ prop.type }}</code></td>
            <td>
@@ -31,10 +32,38 @@ import { Component, Input } from '@angular/core';
        </tbody>
       </table>
     </div>
+    <div class="outputs block-container" *ngIf="classOutputs.length > 0">
+      <p class="block-title">Outputs</p>
+      <table  class="table">
+       <thead>
+         <tr>
+           <td>Name</td>
+           <td>Type</td>
+           <td>Description</td>
+         </tr>
+       </thead>
+       <tbody>
+         <tr *ngFor="let prop of classOutputs">
+           <td>{{ prop.name }}</td>
+           <td><code>{{ prop.type }}</code></td>
+           <td>
+              <p *ngIf="!!prop.shortDescription" ngdDescription>{{ prop.shortDescription }}</p>
+              <p *ngIf="!!prop.description" ngdDescription>{{ prop.description }}</p>
+           </td>
+         </tr>
+       </tbody>
+      </table>
+    </div>  
   `,
 })
 export class NgdPropsBlockComponent {
+  classOutputs: any = [];
+  classInputs: any = [];
 
-  @Input() classData: any;
+  @Input('classData')
+  set setProps(classData: any) {
+    this.classInputs = classData.props.filter(item => item.kind === "input");
+    this.classOutputs = classData.props.filter(item => item.kind === "output");
+  }
 
 }
